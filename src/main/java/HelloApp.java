250d2e405ac4b13763dbdd92b5c72c40d8494958
Naitik Kankariya <naitikj8200@gmail.com>
2026-04-06 15:35:13 +0530

    Coded UC5: Hello with multiple args using enhanced for loop

--- src/main/java/HelloApp.java
+++ src/main/java/HelloApp.java
@@ -1,5 +1,22 @@
 public class HelloApp {
-    public static void main(String[] args){
-        System.out.println("HelloWorld");
+    public static void main(String[] args) {
+
+        StringBuilder names = new StringBuilder();
+
+        // Check if no arguments
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+            return;
+        }
+
+        // Enhanced for loop
+        for (String name : args) {
+            if (names.length() > 0) {
+                names.append(", ");
+            }
+            names.append(name);
+        }
+
+        System.out.println("Hello, " + names + "!");
     }
 }
